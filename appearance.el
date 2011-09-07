bd27a5c0b6f8ea404b4e21faa7e614f00fbc216d
Ranjit Chacko <ranjit.chacko@brightscope.com>
2011-09-06 22:23:40 -0700

    Changed theme.

--- appearance.el
+++ appearance.el
@@ -1,7 +1,7 @@
 (require 'color-theme)
 (load-library "themes/color-theme-library.el")
-(color-theme-tty-dark)
+(color-theme-bharadwaj)
 (add-to-list 'default-frame-alist
              '(font . "-apple-Courier_New-medium-normal-normal-*-*-*-*-*-m-0-iso10646-1"))
-(set-face-attribute 'default nil :height 150)
+(set-face-attribute 'default nil :height 160)
 (blink-cursor-mode t)
