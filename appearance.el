283429582ef7ebbd2af003dc4f89a9811642e650
Ranjit Chacko <ranjit.chacko@brightscope.com>
2012-09-12 21:26:10 -0700

    Small fixes.

--- appearance.el
+++ appearance.el
@@ -1,10 +1,10 @@
-(require 'color-theme)
-(load-library "themes/color-theme-library.el")
-(load-library "themes/color-theme-mac-classic.el")
-(require 'color-theme-solarized)
-(color-theme-solarized-light)
+;(require 'color-theme)
+;(load-library "themes/color-theme-library.el")
+;(load-library "themes/color-theme-mac-classic.el")
+;(require 'color-theme-solarized)
+;(color-theme-solarized-dark)
 
 (add-to-list 'default-frame-alist
              '(font . "-apple-Bitstream_Vera_Sans_Mono-medium-normal-normal-*-*-*-*-*-m-0-iso10646-1"))
-(set-face-attribute 'default nil :height 130)
+(set-face-attribute 'default nil :height 150)
 (blink-cursor-mode t)
--- init.el
+++ init.el
@@ -95,3 +95,9 @@
 (setq cua-enable-cua-keys nil)
 (cua-mode t)
 
+(defun remove-dos-eol ()
+  "Do not show ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M []))
+
